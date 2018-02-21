1bf3628687000e0553e547b4aff2a135121f4e9c
KrasuckiD <darasmm@gmail.com>
2018-02-21 20:14:50 +0100

    some changes

---
+++ src/lekcja2/Loops.java
@@ -0,0 +1,32 @@
+package lekcja2;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Insert number of elements tomultiply");
+        int n = scanner.nextInt();
+            int number = scanner.nextInt();
+            if (number >= 5 &&)
+        for (int i = 0; i < n; i = i + 1){
+            int number = scanner.nextInt();
+            System.out.println((i + 1) + ". Pow(" + number +") =" + (number+number));
+        }
+       // System.out.println("Insert 1 number");
+       // int a = scanner.nextInt();
+       // System.out.println("Insert 2 number");
+       // int b = scanner.nextInt();
+       // System.out.println("Insert 3 number");
+      //  int c = scanner.nextInt();
+       // System.out.println("Insert 4 number");
+       // int d = scanner.nextInt();
+       // int e = scanner.nextInt();
+
+
+        //System.out.println("Sum =" + (a + b + c));
+    }
+}
