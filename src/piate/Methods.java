a5bb8b0d83985cfd90803b042aee857d94cf3035
KrasuckiD <darasmm@gmail.com>
2018-02-28 16:10:11 +0100

    Poprawki

--- src/piate/Methods.java
+++ src/piate/Methods.java
@@ -2,83 +2,39 @@ package piate;
 
 public class Methods {
     public static void main(String[] args) {
-        int[] array= {1,2,3,4,5,6,7,8,9,10};
-        String [] names = {"Ola", "Jacek", "Ala", "Michał", "Wojtek"};
-        System.out.println(femaleNamesCounter(names));
-//        printArray();
-      //  printArray(array);
+        int[] array = {-2,1,4,2,2,3,2,3,3,3,4,6};
+        printArray(array);
+        System.out.println("Sum = " + sumOfArray(array));
+        System.out.println("Avg = " + avg(array));
+        System.out.println("Min = " + min(array));
+        System.out.println("Max = " + max(array));
+        System.out.println("Spread = " + spread(array));
+        printArray(statsOfOccurrences(array));
+        System.out.println("Moda = " + moda(array));
+        printArray(sortByCounting(array));
+    }
 
-        int [] newArray = new int[5];
-//
-//        printArray(newArray);
-//        sumOfArray(newArray);
-//        for (int i = 0; i < newArray.length; i++) {
-//            newArray[i] = i;
-//        }
-//        for (int i = 0; i < newArray.length; i++) {
-//            System.out.println(newArray[i] + " ");
-//
-//        }
-//        System.out.println();
-
-//      // napisac funkcje ktora przejmuje tablice i wyisuje sume
-        // napisac metode zwracajaca najmniejsza wartosc
-        //najwieksza wartosci
-        // informacje o wystapieniach liczb
-        // mediana
-        //rozpietosc
-        // metoda stringow zliczajaca zenskie imiona
-        // metoda zliczajaca litery w stringu ala ma kota
-
-    }
-    public static void printArray(int[] array){
-        System.out.println(sumOfArray(array));
-        System.out.println(avgOfArray(array));
-        System.out.println(smallestElementOfArray(array));
-        System.out.println(biggestElementOfArray(array));
-        System.out.println(spreadOfArray(array));
-        System.out.println(stataOfArray(array));
-    }
-    public static int sumOfArray (int[] array){
-        int sum = 0;
-        for (int i =0; i < array.length; i++){
-            sum += array[i];
-        }
-        return sum;
-    }
-    public static double avgOfArray(int [] array){
-        int sum = sumOfArray(array);
-        return (double)sum / array.length;
-    }
-    public static int smallestElementOfArray (int[] array){
-        int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min){
-                min = array[i];
-            }
-        } return min;
-    }
-    public static int biggestElementOfArray (int[] array) {
-        int max = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+    public static int[] sortByCounting(int[] array) {
+        int[] sortedArray = new int[array.length];
+        int[] stats = statsOfOccurrences(array);
+        int k = 0;
+//        int[] sortedArray = new int[sumOfArray(stats)];
+        for (int i = 0; i < stats.length; i++) {
+            for (int j = 0; j < stats[i]; j++) {
+                sortedArray[k] = i + min(array);
+                k++;
             }
         }
-        return max;
+        return sortedArray;
     }
-    public static int spreadOfArray(int[] array) {
-     return biggestElementOfArray(array) - smallestElementOfArray(array) +1;
+
+    public static int moda(int[] array) {
+        int[] stats = statsOfOccurrences(array);
+        int indexOfMaximumValue = indexOfMax(stats);
+        return indexOfMaximumValue + min(array);
     }
-    public static int [] stataOfArray (int [] array) {
-        int [] statsArray = new int[spreadOfArray(array)];
-        int min = smallestElementOfArray(array);
-        for (int i = 0; i < array.length; i++) {
-            statsArray[array[i] - min]++;
-        }
-        return statsArray;
-    }
-    public static int indexOfMax( int [] array){
+
+    public static int indexOfMax(int[] array) {
         int iMax = 0;
         for (int i = 1; i < array.length; i++) {
             if (array[i] > array[iMax]) {
@@ -87,74 +43,71 @@ public class Methods {
         }
         return iMax;
     }
-//    public static int [] moda (int [] array){
-//        int [] stats = stataOfArray(array);
-//        int indexOfMaximumValue = indexOfMax(stats);
-//        return indexOfMaximumValue + smallestElementOfArray(array);
-//    }
 
-    public static int [] sortByCounting ( int [] array){
-        int[] sortedArray = new int[array.length];
-        int [] stats = stataOfArray(array);
-        int k = 0;
-        for (int i = 0; i < stats.length; i++) {
-            for (int j = 0; j < stats[i]; j++) {
-                sortedArray[k] = i + smallestElementOfArray(array);
-                k++;
-            }
-        }return sortedArray;
+    public static int[] statsOfOccurrences(int[] array) {
+        int[] statsArray = new int[spread(array)];
+        int min = min(array);
+        for (int i = 0; i < array.length; i++) {
+            statsArray[array[i] - min]++;
+        }
+        return statsArray;
     }
-    public static int femaleNamesCounter (String [] names){
-        int counter = 0;
-        for (int i = 0; i < names.length; i++) {
-            if (names[i].endsWith("a")){
-                counter ++;
+
+    public static int spread(int[] array) {
+        return max(array) - min(array) + 1;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
             }
         }
-        return counter;
+        return max;
     }
-//    public static boolean femaleNamesCounter2 (String text, String searchingValue){
-//
-//        if (searchingValue.length() <= text.length()){
-//            boolean result = false;
-//            int i = searchingValue.length() - 1;
-//            char[] textAsCharArray = text.toCharArray();
-//            char[] searchValueAsCharArray = searchingValue.toCharArray();
-//
-//            int aLenth = textAsCharArray.length;
-//            int bLenth = searchValueAsCharArray.length;
-//            while (i >= 0 && (searchValueAsCharArray[i] == textAsCharArray[aLenth + i - bLenth] )){
-//
-//                i --;
-//            } return i == -1;
-//        }
-//            return result;
 
-  //      }
-
-
-        public static int letterCounter (String text, char c){
-        char[] chars = text.toCharArray();
-        int counter = 0;
-        char capitalizedChar = capitalize(c);
-            for (int i = 0; i < chars.length; i--) {
-                if (capitalize(chars[i]) == capitalizedChar){
-                    counter ++;
-                }
-            } return counter;
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
         }
-        public static char capitalize (char c){
-        return (c >= 'a' && c <= 'z') ? (char) (c - 32) : c;
+        return min;
+    }
+
+    //STATYSTYKI
+    //napisac metode ktora zwraca wartosc najmniejszego elementu z tablicy
+    //napisac metode ktora zwraca wartosc najwiekszego elementu z tablicy
+    //mediana
+    //rozpietosc
+    //informacje o wystapieniach liczb
+    //* na podstawie poprzednich metod wypisac posortowana tablice
+
+    public static double avg(int[] array) {
+        int sum = sumOfArray(array);
+        return (double)sum / array.length;
+    }
+
+    // napisac funkcje ktora przyjmuje tablice liczb i wypisuje ich sume
+    public static int sumOfArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
+        return sum;
+    }
 
-
-        public static int lettersCounter2 ( String[] messages, char c){
-        int counter = 0;
-            for (int i = 0; i < messages.length; i++) {
-                counter += letterCounter(messages[i], c);
-            } return counter;
+    public static void printArray(int[] array) {
+//        int[] array = {1,2,3,4,5,6,7,8};
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(";");
+            }
         }
-
-
-
+        System.out.println("]");
+    }
 }
--- src/szoste/Methods.java
+++ src/szoste/Methods.java
@@ -1,100 +1,83 @@
 package szoste;
 
-import czwarte.Warmup;
-
 public class Methods {
+    //silnia (factorial) -> long
+    //zliczanie liczb podzielnych przez 2, ale nie podzielnych przez 3
+    //zamiana jednego znaku na drugi w danym stringu
+    //sprawdzenie czy string jest liczba
+    //czy text jest palindromem np. kajak -> true, anna -> true, abc -> false
+    //split -> 123;abc;dd -> ["123", "abc", "dd"]
+    //         dd.aa.cc -> ["dd", "aa", "cc"]
+    //fibonacci
+    //sprawdzanie nawiasow w stringu
+    //metoda do porownania 2 stringow ktore sa liczbami
+    //metoda wyswietlajaca sume liczb pierwszych w danej liczbie (1,2,3,5,7,11) np.
+    //              liczba 29 = 11 + 11 + 7
+    //              liczba 31 = 11 + 11 + 7 + 2
     public static void main(String[] args) {
-/* silnia (factorial) long
-* zmiana jednego znaku na drugi w danym stringu
-*  sprawdzenie czy w string jest liczba
-*  palindrom np kajak = true, anna = true, ania = false
- * zliczamy liczby podzielne przez 2 a niepodzielne przez 3
-* split --> 123;abc;dd --> "123" "abc" "dd"
-* fibonacci
-* sprawdzenie nawiasow w stringach
-* metoda do porownania dwuch stringow ktore sa liczbami
-* metoda wyswietlajaca sume liczb pierwszych w danej liczbie np 29 = 11 + 11 + 7
-*
-*
-*/
-        System.out.println("Foactorial of 6 = " + factorial(6));
-        int [] array = {1,2,3,4,5,6,7,8,9};
-        System.out.println(" Div by 2 and not for 3 = " + countDivisibleBy2NotBy3(array));
-        System.out.println("Switch text = " + switchChars("Ala ma kota", ' ', '-'));
-        System.out.println("\"123\" is number = " + checkStringIsANumber("123"));
-        System.out.println("\"1b3\" is number = " + checkStringIsANumber("1b3"));
-        System.out.println("\"kajak\" is palindrom = " + isPalindromCheck("kajak"));
-        System.out.println("\"anna\" is palindrom = " + isPalindromCheck("anna"));
-
-
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        System.out.println("Factorial of 6 = " + factorial(6));
+        System.out.println("Divisible by 2 and not by 3 = " + countDivisibleBy2AndNotBy3(array));
+        System.out.println("Switched text = " + switchChars("ala ma kota", 'a', 'b'));
+        System.out.println("\"123\" is number = " + isNumber("123"));
+        System.out.println("\"2a3\" is number = " + isNumber("2a3"));
+        System.out.println("\"anna\" is palindrome = " + isPalindrome("anna"));
+        System.out.println("\"1Kajak1\" is palindrome = " + isPalindrome("1Kajak1"));
+        System.out.println("\"abcdefgh\" is palindrome = " + isPalindrome("abcdefgh"));
     }
 
-    public static long factorial (int number){
-        long result = 1;
-        for (int i = 2; i <= number; i++) {
-            result *= i;
+    //TODO
+    //    public static String[] split(String message, char c) {
+//        String[] array = new String[Warmup.lettersCounter(message, c)];
+//        int start = 0;
+////        int end = getNthIndexOf(c, message, 0);
+//        return array;
+//    }
+
+    public static boolean isPalindrome(String text) {
+        char[] chars = text.toCharArray();
+        int i = 0;
+        while (i < chars.length / 2 && (Warmup.capitalize(chars[i]) == Warmup.capitalize(chars[chars.length - i - 1]))) {
+            i++;
         }
-        return result;
+        return i == chars.length / 2;
     }
-    public static int countDivisibleBy2NotBy3 (int [] array){
-        int counter = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] %2 == 0 && array[i] % 3 != 0 ) {
-                counter++;
-            }
+
+    public static boolean isNumber(String numberCandidate) {
+        char[] chars = numberCandidate.toCharArray();
+        int i = 0;
+        while (i < chars.length && (chars[i] >= '0' && chars[i] <= '9')) {
+            i++;
         }
-        return counter;
+        return i == chars.length;
     }
-    public static String switchChars(String text, char from, char to){
-        char [] textAsChars = text.toCharArray();
+
+    public static String switchChars(String text, char from, char to) {
+        char[] textAsChars = text.toCharArray();
         for (int i = 0; i < textAsChars.length; i++) {
-            if (textAsChars[i] == from){
+            if (textAsChars[i] == from) {
                 textAsChars[i] = to;
             }
         }
         return String.valueOf(textAsChars);
     }
 
-
-    public static boolean checkStringIsANumber (String numberCandidate ){
-        char[] chars =numberCandidate.toCharArray();
-        int i = 0;
-        while (i < chars.length && (chars[i] >= '0' && chars[i] <= '9')){
-            i++;
+    public static int countDivisibleBy2AndNotBy3(int[] array) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0 && array[i] % 3 != 0) {
+                counter++;
+            }
+//            counter += (array[i] % 2 == 0 && array[i] % 3 != 0) ? 1 : 0;
         }
-//        if (i == chars.length){
-//            return true;
-//        } else {
-//            return false;
-//        }
-        return i == chars.length;
+        return counter;
     }
 
-    public static boolean isPalindromCheck (String textToPalindromCheck){
-        char [] palindromChars = textToPalindromCheck.toCharArray();
-        int i = 0;
-        while (i < palindromChars.length / 2 && palindromChars[i] == palindromChars[palindromChars.length - i - 1]){
-            i++;
+    public static long factorial(int number) {
+        long result = 1;
+        for (int i = 2; i <= number; i++) {
+            result *= i;
         }
-
-
-
-        return i == palindromChars.length / 2;
+        return result;
     }
-
-
-    public static String split (String message, char c) {
-        String [] array = new String[piate.Methods.letterCounter(message, c)];{
-            int start = 0;
-            int end = getNthIndexOf (c, message, 0);
-
-
-
-            return array;
-        }
-
-
-        return
-    }
-
 }
